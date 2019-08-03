8962694d91e58b76b6078820fa4c53ed6ab167fa
MalakaJayawardhana <malaka.jayaw@gmail.com>
2019-08-03 16:52:57 +0530

    test

--- app/src/main/java/com/example/myapplication/MAddHSummary.java
+++ app/src/main/java/com/example/myapplication/MAddHSummary.java
@@ -6,17 +6,16 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+public class MAddHSummary extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_madd_hsummary);
     }
+
     public void onclick(View view){
         Intent a = new Intent(this,MHome.class);
         startActivity(a);
     }
-
-
 }
