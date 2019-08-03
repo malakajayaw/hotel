2aeb575443a11130ea13cae18d82c372e33ffcea
Dilhan Weerakoon <weerakoondilshan.dw@gmail.com>
2019-08-03 16:24:12 +0530

    testing

--- app/src/main/java/com/example/myapplication/ui/TchangePwd.java
+++ app/src/main/java/com/example/myapplication/ui/TchangePwd.java
@@ -4,11 +4,14 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.example.myapplication.R;
+
 public class TchangePwd extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tchange_pwd);
         setContentView(R.layout.activity_tchange_pwd);
     }
 }
