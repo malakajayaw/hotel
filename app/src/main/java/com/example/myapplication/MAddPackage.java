d3c954f2909fdac329802c06f8241543378f2e90
MalakaJayawardhana <malaka.jayaw@gmail.com>
2019-08-03 12:26:35 +0530

    test

---
+++ app/src/main/java/com/example/myapplication/MAddPackage.java
@@ -0,0 +1,14 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MAddPackage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_madd_package);
+    }
+}
